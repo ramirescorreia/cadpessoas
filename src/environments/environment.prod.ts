2b3ba7fc75c1605f3667065d94084f89ad4ce949
Ramiro Correia V. Cardoso <ramirojavadeveloper@gmail.com>
2020-08-18 15:00:34 -0300

    Criação de url produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/cadastropessoas-api'
+  baseUrl: 'http://ec2-54-196-22-85.compute-1.amazonaws.com:8080/cadastropessoas-api'
 };
